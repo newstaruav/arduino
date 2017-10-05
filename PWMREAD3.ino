6b15cd2871816cab04ccef28320e86c828047e88
newstaruav <32448227+newstaruav@users.noreply.github.com>
2017-10-05 09:24:46 +0800

    Add files via upload

---
+++ PWMREAD3.ino
@@ -0,0 +1,18 @@
+int rssiDur;
+
+void setup() {
+Serial.begin(9600);
+unsigned long rssiDur;
+float percentage;
+
+}
+
+void loop() {
+  rssiDur = pulseIn(5, HIGH, 200);
+  percentage=rssiDur / 64;
+Serial.print("PWM raw signal:   ");
+Serial.println(rssiDur);
+Serial.print("PWM Percentage:   ");
+Serial.println(percentage);
+delay(1000);
+}
